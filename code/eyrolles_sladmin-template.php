488370f2cb7bda0b50f339b5be7d91ba547eb2ed
Jérémy Subtil <bigmadwolf@gmail.com>
2010-01-18 00:25:11 +0100

    Listings modifiés.

---
+++ code/eyrolles_sladmin-template.php
@@ -0,0 +1,4 @@
+<?php echo sfOutputEscaper::unescape($widgetActionListFilter)->render() ?>
+<?php echo sfOutputEscaper::unescape($widgetPagerInfos)->render() ?>
+<?php echo sfOutputEscaper::unescape($widgetPager)->render() ?>
+<?php echo sfOutputEscaper::unescape($widgetTable)->render() ?>
